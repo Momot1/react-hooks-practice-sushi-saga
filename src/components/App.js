5520d56a0c06ec79ecd87f29a5be608e43e86ec5
Mark Momot <markmomot03@gmail.com>
2021-11-19 13:09:52 -0500

    commit tree 786ff9ff78e46338bde9889dae62e01656b13947

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
-const API = "http://localhost:3001/sushis";
+// const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [balance, setBalance] = useState(100)
+  const [plates, setPlates] = useState([])
+
+  console.log(balance)
+  function setNewBalance(price){
+    setBalance(balance-price)
+    setPlates([...plates, price])
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sendSushiUp={setNewBalance} balance={balance}/>
+      <Table balance = {balance} plates={plates}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({sendSushiUp, balance}) {
+  const [sushiAry, setSushiAry] = useState([])
+  const [page, setPage] = useState(1)
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/sushis/?_page=${page}&_limit=4`)
+      .then(resp => resp.json())
+      .then(data => setSushiAry(data))
+  }, [page])
+
+  const sushiElements = sushiAry.map(sushi => <Sushi key={sushi.id} name={sushi.name} image={sushi.img_url} price={sushi.price} onSushiClick={handleSushiClick} balance={balance}/>)
+
+  function changePage(){
+    setPage(page+1)
+  }
+
+  function handleSushiClick(price){
+    sendSushiUp(price)
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiElements}
+      <MoreButton onChangePage={changePage}/>
     </div>
   );
 }
